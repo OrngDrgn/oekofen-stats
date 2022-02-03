c7260354f3174b99c9ef35056f51e459d4da5fc8
Orange Dragon <orng.drgn@gmail.com>
2022-02-03 22:12:57 +0100

    enable scheduling

--- oekofen-stats-collector/src/main/java/orng/drgn/oekofen/client/PelletronicClient.java
+++ oekofen-stats-collector/src/main/java/orng/drgn/oekofen/client/PelletronicClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import orng.drgn.oekofen.repo.Metric;
 
-@FeignClient(name = "${orng.drgn.oekofen.url}/${orng.drgn.oekofen.password}")
+@FeignClient(name = "oekofen", url = "${orng.drgn.oekofen.url}/${orng.drgn.oekofen.password}")
 public interface PelletronicClient {
 
 	@GetMapping(path = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
